5a654149493894ccfc4af81d0ea87c1fd6a9644b
Kezia Meilany Tandapai <keziameilany2005@gmail.com>
2026-03-14 22:00:19 +0700

    Initial Commit

---
+++ OtherEyes/OtherEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtherEyesApp.swift
+//  OtherEyes
+//
+//  Created by Kezia Meilany Tandapai on 14/03/26.
+//
+
+import SwiftUI
+
+@main
+struct OtherEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
